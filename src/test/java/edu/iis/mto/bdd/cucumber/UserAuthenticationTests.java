2390a6d88c029f3e603c698be4af836432b379e4
Maciej Sulej <187809@edu.p.lodz.pl>
2016-06-10 00:15:29 +0200

    Zmiana Runnera na CucumberWithSerenity

--- src/test/java/edu/iis/mto/bdd/cucumber/UserAuthenticationTests.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/UserAuthenticationTests.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(plugin = { "pretty",
 		"html:target/cucumber" },  tags = {"@authentication"}, glue = "edu.iis.mto.bdd.cucumber.steps")
 public class UserAuthenticationTests {
